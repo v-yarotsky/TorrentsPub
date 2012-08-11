95d503451a18e2345d738352cf42228be7cf9c6c
Vladimir <v.yarotsky@itransition.com>
2012-08-11 03:01:57 +0300

    vendor js fix

--- assets/vendor/javascripts/vendor.js
+++ assets/vendor/javascripts/vendor.js
@@ -1,5 +1,5 @@
-//= require jquery-1.7.2
-//= require json2
-//= require underscore
-//= require backbone
+//= require ./jquery-1.7.2
+//= require ./json2
+//= require ./underscore
+//= require ./backbone
 
